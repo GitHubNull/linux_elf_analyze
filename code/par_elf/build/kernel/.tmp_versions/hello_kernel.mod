20af5b99a0d268d4512e7afed9a5c3f4aba1e975
githubnull <641570479@qq.com>
2017-04-23 23:12:39 +0800

    add par_elf project and ignore info.

---
+++ code/par_elf/build/kernel/.tmp_versions/hello_kernel.mod
@@ -0,0 +1,2 @@
+/home/lm/桌面/test/hello2/src/kernel/hello_kernel.ko
+/home/lm/桌面/test/hello2/src/kernel/hello_kernel.o
---
+++ code/par_elf/init_pro.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "init project"
+mkdir -p src/user src/kernel include/user include/kernel build/user build/kernel doc/user doc/kernel /scripts
+echo "init project OK."
